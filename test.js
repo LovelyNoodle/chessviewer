e028c0c1e8f997866157d0902004a853e0b9535c
victorocna <victorocnarescu@gmail.com>
2020-07-30 14:26:24 +0300

    update project skeleton

--- test.js
+++ test.js
@@ -1,16 +1,39 @@
 const chessviewer = require('./main');
 const expect = require('chai').expect;
 
-it('works with the simplest pgn ever', function() {
+describe.only('basic example', function () {
   // Arrange
-  const pgn = '1. e4 e5';
+  const pgn = '1. e4';
 
   // Act
-  const result = chessviewer(pgn);
-  const first = result[0];
+  const moments = chessviewer(pgn);
 
   // Assert
-  expect(result.length).to.equal(2);
-  expect(first.move).to.equal('e4');
-  expect(first.fen).to.equal('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1');
+  expect(moments.length).to.equal(1);
+  expect(moments[0].move).to.equal('e4');
+  expect(moments[0].fen).to.equal('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1');
+});
+
+describe('invalid PGN', function () {
+  // what happens when an invalid PGN is passed
+});
+
+describe('with comments', function () {
+  // test only basic functionality + comments
+});
+
+describe('with special symbols, also called NAGs', function () {
+  // test only basic functionality + NAGs
+});
+
+describe('with shapes', function () {
+  // test only basic functionality + shapes
+});
+
+describe('with variations', function () {
+  // test only basic functionality + variations
+});
+
+describe('everything put together', function () {
+  // test everything
 });
